9a8fc716c21ee43bcd77450826d95115c98f59a1
KaZuKuTa99 <abelgsolis@gmail.com>
2022-01-12 15:41:44 -0600

    Added welcome message

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const TOKEN = "OTMwMjQ1NTMwNjM2OTIyOTQw.YdzEqQ.ILba7rWKPZdVrSWaToBjVKpg5sc"
 const client = new Discord.Client({
 	intents: [
 		"GUILDS",
-		"GUILD_MESSAGES"
+		"GUILD_MESSAGES",
+		"GUILD_MEMBERS"
  	]
 })
 
@@ -15,9 +16,13 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-	if (message.content == "hi") {
-		message.reply("Hello numbnuts! >:)")
+	if (message.content == "hi kouko" || message.content == "hello kouko") {
+		message.reply(`Hello User-chan`)
 	}
 })
 
+const welcomeChannelId = "930605509885694062"
+client.on("guildMemberAdd", (member) => {
+	member.guild.channels.cache.get(welcomeChannelId).send(`Oh, you decided to actually come here <@${member.id}>?`)
+})
 client.login(process.env.TOKEN)
